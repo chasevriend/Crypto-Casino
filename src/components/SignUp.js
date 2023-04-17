262684732c2e480b476dac399efce696dfd99374
NEWUSER <chasevriend84@gmail.com>
2023-04-17 11:08:28 -0700

    new commit

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,4 @@
 import React from "react";
-import hero from "../images/hero-video.mp4";
 import "../css/SignUp.css";
 
 const SignUp = () => {
@@ -8,11 +7,7 @@ const SignUp = () => {
             <div className="signUp__content">
                 <h1>Buy, Sell & Trade CryptoCurrencies</h1>
                 <p>Securely buy crypto and earn rewards with staking. Sign up to trade & gamble today.</p>
-            </div>
-            <div className="signUp__graphic">
-                <hero width="750" height="500" controls >
-                    
-                </hero>
+                <button>Sign Up</button>
             </div>
         </div>
     )
