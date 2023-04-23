5baaf789b9b6986ad1a0e58f6579d3d439cec246
NEWUSER <chasevriend84@gmail.com>
2023-04-23 07:42:56 -0700

    new commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import CasinoIcon from '@mui/icons-material/Casino';
 import "../css/Header.css";
 
 const Header = () => {
+    
+    const [sticky, setSticky] = useState(false);
+    const [mobile, setMobile] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 150) {
+            setSticky(true);
+        } else {
+            setSticky(false);
+        }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    const goToTop = () => {
+        window.scrollTo({
+            top: (0, 0),
+            behavior: "smooth",
+        });
+    };
+
+    const openMobile = () => {
+        setMobile(!mobile);
+    }
+    
+    
     return(
         <div className="header">
             <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
@@ -13,11 +39,11 @@ const Header = () => {
                 </div>
             </Link>
             <div className="header__btns">
-                {/* <Link to='/markets' style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <div className="header__btn">
-                        <h4>Markets</h4>
-                    </div>
-                </Link> */}
+                <ul>
+                    <li><a href="#/">Home</a></li>
+                    <li><a href="#/Market">Market</a></li>
+                    <li><a href="#/WhyUs">Choose Us</a></li>
+                </ul>
                 <button className="header__joinBtn">Sign Up</button>
             </div>
         </div>
